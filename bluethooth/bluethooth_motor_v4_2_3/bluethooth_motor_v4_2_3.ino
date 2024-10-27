deeb6b2b1ca6d5d06417e257616fd152742167f9
Enesdagci <162743017+Enesdagci@users.noreply.github.com>
2024-10-27 16:51:24 +0300

    Updated Engine Code
    Changed the pins.

--- bluethooth/bluethooth_motor_v4_2_3/bluethooth_motor_v4_2_3.ino
+++ bluethooth/bluethooth_motor_v4_2_3/bluethooth_motor_v4_2_3.ino
@@ -3,9 +3,9 @@ BluetoothSerial Serial_BT;
 
 // Motor 1
 // D0 - PWM, D1 - IN1, D2 - IN2
-#define ENA D2
-#define IN1 D0
-#define IN2 D1
+#define ENA D0
+#define IN1 D1
+#define IN2 D2
 int data;
 
 void setup() {
@@ -15,44 +15,21 @@ void setup() {
   // digitalWrite(D0, HIGH);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
-
-  // Ekrana yazdırma ile bluetooth aktifliğini kontrolü
-  Serial.begin(9600);
-  if (!Serial_BT.begin("Hamal")) {
-    Serial.println("Kullanım dışı ....");
-    while (1)
-      ;
-  }
-  Serial.println("Bağlantı Hazır ....");
-}
+  Serial_BT.begin("Hamal");
 
 void loop() {
   if (Serial_BT.available()) {
     data = Serial_BT.read();
-    Serial.print("Data: ");
-    Serial.println(data);
     switch (data) {
       case 'F':
-       // delay(100);
-        //digitalWrite(D1,LOW);
-        //digitalWrite(D2,LOW);
-
-        //delay(100);
-        
         digitalWrite(IN2, HIGH);
         digitalWrite(IN1, LOW);
-        Serial.println("İleri ....");
         break;
       case 'B':
-       // digitalWrite(D1,LOW);
-      //  digitalWrite(D2,LOW);
-      //  delay(100);
         digitalWrite(IN2, LOW);
         digitalWrite(IN1, HIGH);
-        Serial.println("Geri ....");
         break;
       default:
-        Serial.println("Durdu ....");
         digitalWrite(IN2, LOW);
         digitalWrite(IN1, LOW);
         break;
